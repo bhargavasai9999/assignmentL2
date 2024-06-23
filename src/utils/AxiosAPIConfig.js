dcd914282b4bae23f96cddd4cf923976bb0720f2
Bhargava Sai <bhargavasai.rayalla@gmail.com>
2024-06-23 21:40:02 +0530

    api fix

--- src/utils/AxiosAPIConfig.js
+++ src/utils/AxiosAPIConfig.js
@@ -3,7 +3,7 @@ const api=axios.create({
     baseURL:'https://api.unsplash.com',
     headers:{
         "Accept-Version": 'v1',
-        "Authorization": `Client-ID RgwwpsIULZgawKXH1kUGSHT672KgPj8CPz6KXxQzM18`
+        "Authorization": `Client-ID YOUR_ACCESS_KEY`
     }
     
 })
